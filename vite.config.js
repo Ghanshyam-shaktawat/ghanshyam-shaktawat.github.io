91998aacc368b7dc1d3d22cecc20216eb4dc3ea1
Ghanshyam Singh <ghan6171@gmail.com>
2023-11-07 14:31:39 +0530

    React Base

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-export default {
-    root: 'src/',
-    publicDir: '../public/',
-    base: './',
-    server:{
-        fs: {
-            allow: ['/']
-        }
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
